4ef60914dddba12e5c996b09053fe7f37ea4d6b1
Ryan Sherrill <ryanbsherrill@gmail.com>
2017-04-03 23:40:03 -0400

    js-cleaned-up

--- app.js
+++ app.js
@@ -5,18 +5,11 @@ var YOUTUBE_BASE_URL = "https://www.googleapis.com/youtube/v3/search";
 var parameters = {
 	part: 'snippet',
 	key: 'AIzaSyAvVDxP5JbEej3bMbeCftybGlcTe34tBCQ',
-	q: '',
 	type: 'video'
-};
-
-// callback
-var displayData = function(data){
-	console.log(data);
 };
 
 // create clickable images
 var displayResults = function(data){
-	console.log(data.items);
 	data.items.forEach(function(item){
 		$('.js-search-results').append(
 			'<a href="https://www.youtube.com/watch?v=' + item.id.videoId + 
@@ -37,47 +30,3 @@ $('.js-button').on('click', function(e) {
 	parameters.q = $('.js-input').val();
 	getData();
 });
-
-
-// request & response
-// XML HTTP Request Object = XHR
-// extensible markup language
-
-// create request object
-// define a callback function
-// open a request
-// send the request
-
-/*
-const YOUTUBE_BASE_URL = "https://www.googleapis.com/youtube/v3/search";
-
-let parameters = {
-	part: 'snippet',
-	key: 'AIzaSyAvVDxP5JbEej3bMbeCftybGlcTe34tBCQ',
-	q: ('horse'),
-}
-
-let search = () => $.getJSON(YOUTUBE_BASE_URL, params, displayData);
-
-let displayData = (data) => data;
-
-search()
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
